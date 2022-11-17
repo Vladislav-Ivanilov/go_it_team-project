c21c3586a1a303859cb2480dd77d37cb56edf5fe
Vladislav-Ivanilov <62110387+Vladislav-Ivanilov@users.noreply.github.com>
2022-11-17 19:17:40 +0000

    Deploying to gh-pages from @ Vladislav-Ivanilov/go_it_team-project@6059154d638049d105f7cfbd5260780496f36652 🚀

---
+++ index.835d28cd.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),n=document.querySelector("body");e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),n.classList.toggle("is-hidden"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.835d28cd.js.map
